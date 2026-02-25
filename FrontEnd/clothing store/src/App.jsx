bdd39c280a5cac31e20d78dd37ecd6b28fdda217
chavi-gif <chavinisha11@gmail.com>
2026-02-25 19:45:21 +0530

    updated

--- FrontEnd/clothing store/src/App.jsx
+++ FrontEnd/clothing store/src/App.jsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+
+import LoginPage from "./Pages/login";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      
-    </>
-  )
+  return <LoginPage />;
 }
 
-export default App
+export default App;
