50816ae6d43c4fc7336be9177f88ecbdfe949cdb
Maheesha nidushani <maheeshaecc18@gmail.com>
2026-04-10 12:33:59 +0530

    create contact us page

--- FrontEnd/clothing store/src/App.jsx
+++ FrontEnd/clothing store/src/App.jsx
@@ -3,31 +3,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from "./Pages/home";
 import Cart from "./Pages/cart";
 import CheckoutPage from "./Pages/checkout";
-import LoginPage from "./Pages/login";
-import RegisterPage from "./Pages/register";
-import ForgotPage from "./Pages/forgot";
-import Dashboard from "./Pages/admin_dashboard1";
+import ContactUs from "./Pages/Contactus";
+
 
 function App() {
-  const [page, setPage] = useState("home");
-  const [recoveryMsg, setRecoveryMsg] = useState("");
-
-  const handleForgotSuccess = () => {
-    setRecoveryMsg("We've sent you an email with a link to update your password.");
-    setPage("login");
-  };
-
-  if (page === "dashboard") return <Dashboard />;
-  if (page === "register")  return <RegisterPage onNavigate={setPage} />;
-  if (page === "forgot")    return <ForgotPage onNavigate={setPage} onSuccess={handleForgotSuccess} />;
-  if (page === "login")     return <LoginPage onLogin={() => setPage("dashboard")} onNavigate={setPage} recoveryMsg={recoveryMsg} />;
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/"         element={<HomePage />} />
         <Route path="/cart"     element={<Cart />} />
         <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/Contactus" element={<ContactUs />} />
       </Routes>
     </BrowserRouter>
   );
