6ff686c826d65751a26ffdd227da745851caa185
Maheesha nidushani <maheeshaecc18@gmail.com>
2026-05-05 00:34:26 +0530

    Refactor Customer model by updating associations and fields
    Removed the association with OrderDetail and updated field mappings for firstName and lastName.

--- BackEnd/models/customer.js
+++ BackEnd/models/customer.js
@@ -6,59 +6,63 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       Customer.belongsTo(models.User, { foreignKey: 'userId' });
       Customer.hasMany(models.Order, { foreignKey: 'customerId' });
-      Customer.hasMany(models.OrderDetail, { foreignKey: 'customerId' });
     }
   }
-  
+
   Customer.init({
     userId: {
       type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'users',
-        key: 'id'
-      }
+      allowNull: false
     },
+
     firstName: {
       type: DataTypes.STRING(100),
       allowNull: false,
-      field: 'fname'
+      field: 'first_name' // maps DB column if needed
     },
+
     lastName: {
       type: DataTypes.STRING(100),
       allowNull: false,
-      field: 'lname'
+      field: 'last_name'
     },
+
     email: {
       type: DataTypes.STRING(100),
       allowNull: false
     },
+
     address: {
       type: DataTypes.STRING(255),
       allowNull: true
     },
+
     city: {
       type: DataTypes.STRING(100),
       allowNull: true
     },
+
     district: {
       type: DataTypes.STRING(100),
       allowNull: true
     },
+
     province: {
       type: DataTypes.STRING(100),
       allowNull: true
     },
+
     phone: {
       type: DataTypes.STRING(20),
       allowNull: true
     }
+
   }, {
     sequelize,
     modelName: 'Customer',
     tableName: 'customers',
     timestamps: true
   });
-  
+
   return Customer;
 };
