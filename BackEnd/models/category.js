ac530fff783041a698c83f242cc4244345373dd3
Maheesha nidushani <maheeshaecc18@gmail.com>
2026-05-04 20:38:08 +0530

    Remove 'id' field from Category model
    Removed the 'id' field definition from the Category model.

--- BackEnd/models/category.js
+++ BackEnd/models/category.js
@@ -6,15 +6,11 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       Category.hasMany(models.Product, { foreignKey: 'categoryId' });
       Category.hasMany(models.ProductWithSizeColor, { foreignKey: 'categoryId' });
+      Category.hasMany(models.Return, { foreignKey: 'categoryId' });
     }
   }
   
   Category.init({
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true
-    },
     name: {
       type: DataTypes.STRING(50),
       allowNull: false,
