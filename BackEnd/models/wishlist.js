e060c3ad123ea2dfd8d2558d13b2a89cb565431d
chavi-gif <chavinisha11@gmail.com>
2026-05-04 19:04:29 +0530

    Update wishlist.js

--- BackEnd/models/wishlist.js
+++ BackEnd/models/wishlist.js
@@ -5,6 +5,7 @@ module.exports = (sequelize, DataTypes) => {
   class Wishlist extends Model {
     static associate(models) {
       Wishlist.belongsTo(models.User, { foreignKey: 'userId', as: 'user' });
+      Wishlist.belongsTo(models.Product, { foreignKey: 'productId', as: 'product' });  // ← ADD THIS
     }
   }
 
