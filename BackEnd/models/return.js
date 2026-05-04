aee3d5f4b98431732a5d6c5cfd355055a43f5d8d
Maheesha nidushani <maheeshaecc18@gmail.com>
2026-05-05 00:40:53 +0530

    Refactor Return model to simplify structure

--- BackEnd/models/return.js
+++ BackEnd/models/return.js
@@ -1,58 +1,29 @@
-'use strict'; 
+'use strict';
 const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   class Return extends Model {
     static associate(models) {
+      Return.belongsTo(models.User, { foreignKey: 'userId' });
+      Return.belongsTo(models.Order, { foreignKey: 'orderId' });
       Return.belongsTo(models.Product, { foreignKey: 'productId' });
-      Return.belongsTo(models.Category, { foreignKey: 'categoryId' });
     }
   }
-  
+
   Return.init({
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true
-    },
-    productId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'products',
-        key: 'id'
-      }
-    },
-    categoryId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'categories',
-        key: 'id'
-      }
-    },
-    size: {
-      type: DataTypes.STRING(10),
-      allowNull: false
-    },
-    color: {
-      type: DataTypes.STRING(30),
-      allowNull: false
-    },
-    quantity: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    reason: {
-      type: DataTypes.TEXT,
-      allowNull: true
-    }
+    userId: DataTypes.INTEGER,
+    orderId: DataTypes.INTEGER,
+    productId: DataTypes.INTEGER,
+    size: DataTypes.STRING,
+    color: DataTypes.STRING,
+    quantity: DataTypes.INTEGER,
+    reason: DataTypes.TEXT
   }, {
     sequelize,
     modelName: 'Return',
     tableName: 'returns',
     timestamps: true
   });
-  
+
   return Return;
 };
