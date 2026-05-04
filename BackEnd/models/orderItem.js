5d4b8bee881a6ca47cfd4bf2662e4124d71901e3
chavi-gif <chavinisha11@gmail.com>
2026-05-04 18:55:02 +0530

    Create orderItem.js

---
+++ BackEnd/models/orderItem.js
@@ -0,0 +1,60 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class OrderItem extends Model {
+    static associate(models) {
+      OrderItem.belongsTo(models.Order, { foreignKey: 'orderId' });
+      OrderItem.belongsTo(models.Product, { foreignKey: 'productId' });
+    }
+  }
+  
+  OrderItem.init({
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    orderId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'orders',
+        key: 'id'
+      },
+      field: 'order_id'
+    },
+    productId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'products',
+        key: 'id'
+      },
+      field: 'product_id'
+    },
+    size: {
+      type: DataTypes.STRING(10),
+      allowNull: false
+    },
+    color: {
+      type: DataTypes.STRING(30),
+      allowNull: false
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'OrderItem',
+    tableName: 'order_items',
+    timestamps: true
+  });
+  
+  return OrderItem;
+};
