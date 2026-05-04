179d9d5c5b509841453bc82dbc2f335c869a2f75
chavi-gif <chavinisha11@gmail.com>
2026-05-04 18:53:40 +0530

    Create customer.js

---
+++ BackEnd/models/customer.js
@@ -0,0 +1,68 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Customer extends Model {
+    static associate(models) {
+      Customer.belongsTo(models.User, { foreignKey: 'userId' });
+      Customer.hasMany(models.Order, { foreignKey: 'customerId' });
+    }
+  }
+  
+  Customer.init({
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'users',
+        key: 'id'
+      }
+    },
+    firstName: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      field: 'fname'
+    },
+    lastName: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      field: 'lname'
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false
+    },
+    address: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    city: {
+      type: DataTypes.STRING(100),
+      allowNull: true
+    },
+    district: {
+      type: DataTypes.STRING(100),
+      allowNull: true
+    },
+    province: {
+      type: DataTypes.STRING(100),
+      allowNull: true
+    },
+    phone: {
+      type: DataTypes.STRING(20),
+      allowNull: true
+    }
+  }, {
+    sequelize,
+    modelName: 'Customer',
+    tableName: 'customers',
+    timestamps: true
+  });
+  
+  return Customer;
+};
