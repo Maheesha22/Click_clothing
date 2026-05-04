8b5e43c9419fe988250d1163d017d70fefe70da9
chavi-gif <chavinisha11@gmail.com>
2026-05-04 19:01:38 +0530

    Update return.js

--- BackEnd/models/return.js
+++ BackEnd/models/return.js
@@ -21,8 +21,7 @@ module.exports = (sequelize, DataTypes) => {
       references: {
         model: 'products',
         key: 'id'
-      },
-      field: 'product_id'
+      }
     },
     categoryId: {
       type: DataTypes.INTEGER,
@@ -30,8 +29,7 @@ module.exports = (sequelize, DataTypes) => {
       references: {
         model: 'categories',
         key: 'id'
-      },
-      field: 'category_id'
+      }
     },
     size: {
       type: DataTypes.STRING(10),
