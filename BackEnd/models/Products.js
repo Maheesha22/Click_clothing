df9634ed3e5c6274fdf48b061064edabab86b793
Maheesha nidushani <maheeshaecc18@gmail.com>
2026-05-05 00:38:08 +0530

    Refactor Product model associations and fields

--- BackEnd/models/Products.js
+++ BackEnd/models/Products.js
@@ -1,48 +1,32 @@
-'use strict'; 
+'use strict';
 const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   class Product extends Model {
     static associate(models) {
-      Product.belongsTo(models.Category, { foreignKey: 'categoryId' });
-      Product.hasMany(models.ProductWithSizeColor, { foreignKey: 'productId' });
-      Product.hasMany(models.OrderItem, { foreignKey: 'productId' });
-      Product.hasMany(models.Wishlist, { foreignKey: 'productId' });
+      Product.belongsTo(models.Category, {
+        foreignKey: 'categoryId',
+        as: 'category'
+      });
+
+      Product.hasMany(models.ProductVariant, {
+        foreignKey: 'productId',
+        as: 'variants'
+      });
     }
   }
-  
+
   Product.init({
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true
-    },
-    name: {
-      type: DataTypes.STRING(100),
-      allowNull: false
-    },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: true
-    },
-    price: {
-      type: DataTypes.DECIMAL(10, 2),
-      allowNull: false
-    },
-    categoryId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'categories',
-        key: 'id'
-      }
-    }
+    name: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    price: DataTypes.DECIMAL(10,2),
+    categoryId: DataTypes.INTEGER
   }, {
     sequelize,
     modelName: 'Product',
     tableName: 'products',
     timestamps: true
   });
-  
+
   return Product;
 };
