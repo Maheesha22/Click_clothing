e5e6760c2adff510a5b432a34519d3b53a51ab02
chavi-gif <chavinisha11@gmail.com>
2026-05-06 13:25:55 +0530

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -29,11 +29,15 @@ const userRoutes    = require("./routes/UserRoutes");
 const productRoutes = require("./routes/productRoutes");   // ← added
 const cartRoutes = require("./routes/CartRoutes");
 const contactRoutes = require('./routes/contactRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+const returnRoutes = require('./routes/returnRoutes');
 
 app.use("/api/users",    userRoutes);
 app.use("/api/products", productRoutes);                   // ← added
 app.use("/api/cart", cartRoutes);
 app.use("/api/contact", contactRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/returns", returnRoutes);
 
 const wishlistRoutes = require("./routes/WishlistRoutes");
 app.use("/api/wishlist", wishlistRoutes);
