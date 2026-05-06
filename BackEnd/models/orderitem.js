9854e1bf634191b9c0c32965bcdf7f71ff4a4069
Maheesha nidushani <maheeshaecc18@gmail.com>
2026-05-07 02:51:42 +0530

    Update OrderItem model structure and associations
    Refactor OrderItem model to include userId and update table name.

--- BackEnd/models/orderitem.js
+++ BackEnd/models/orderitem.js
@@ -4,52 +4,34 @@ const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class OrderItem extends Model {
     static associate(models) {
-      OrderItem.belongsTo(models.Order, { foreignKey: 'orderId' });
-      OrderItem.belongsTo(models.Product, { foreignKey: 'productId' });
+      OrderItem.belongsTo(models.Order, {
+        foreignKey: 'orderId'
+      });
+
+      OrderItem.belongsTo(models.Product, {
+        foreignKey: 'productId'
+      });
+
+      OrderItem.belongsTo(models.User, {
+        foreignKey: 'userId'
+      });
     }
   }
-  
+
   OrderItem.init({
-    orderId: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    productId: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    productName: {
-      type: DataTypes.STRING(200),
-      allowNull: false
-    },
-    size: {
-      type: DataTypes.STRING(20),
-      allowNull: true
-    },
-    color: {
-      type: DataTypes.STRING(50),
-      allowNull: true
-    },
-    quantity: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 1,
-      validate: {
-        min: 1
-      }
-    },
-    price: {
-      type: DataTypes.DECIMAL(10, 2),
-      allowNull: false,
-      validate: {
-        min: 0
-      }
-    }
+    orderId: DataTypes.INTEGER,
+    userId: DataTypes.INTEGER,
+    productId: DataTypes.INTEGER,
+    size: DataTypes.STRING,
+    color: DataTypes.STRING,
+    quantity: DataTypes.INTEGER,
+    price: DataTypes.DECIMAL(10,2)
   }, {
     sequelize,
     modelName: 'OrderItem',
-    tableName: 'OrderItems'
+    tableName: 'order_items',
+    timestamps: true
   });
-  
+
   return OrderItem;
 };
