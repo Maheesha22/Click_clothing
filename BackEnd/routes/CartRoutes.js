314cdb1aa323c499b373bbff747a3e9aee86b242
Maheesha nidushani <maheeshaecc18@gmail.com>
2026-05-07 02:53:02 +0530

    Fix route for updating cart items

--- BackEnd/routes/CartRoutes.js
+++ BackEnd/routes/CartRoutes.js
@@ -5,8 +5,8 @@ const CartController = require('../controllers/CartController');
 // Cart routes
 router.post('/add', CartController.addToCart);
 router.get('/:userId', CartController.getCart);
-router.put('/update-item/:cartId', CartController.updateCartItem);
 router.put('/update/:cartId', CartController.updateQuantity);
+router.put('/update-item/:cartId', CartController.updateCartItem);
 router.delete('/remove/:cartId', CartController.removeFromCart);
 router.delete('/clear/:userId', CartController.clearCart);
 
