f0102b2943cf94c483bfcb0062624bec716384ea
Maheesha nidushani <maheeshaecc18@gmail.com>
2026-05-08 02:15:34 +0530

    Update static file serving to use path.join

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const cors    = require('cors');
+const path  = require('path');
 const app     = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
