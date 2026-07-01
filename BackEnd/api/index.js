e53f391d692dd2f721ae6ed4a1615ebb06a6bc3d
Maheesha nidushani <maheeshaecc18@gmail.com>
2026-07-01 16:34:58 +0530

    cd

--- BackEnd/api/index.js
+++ BackEnd/api/index.js
@@ -1,3 +1,7 @@
+// Force-require mysql2 before anything else so Vercel's bundler includes it
+require('mysql2');
+require('mysql2/promise');
+
 const app = require('../index');
 
 module.exports = app;
