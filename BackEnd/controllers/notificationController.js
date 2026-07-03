ab4b3c35969114187e23ef400da1c226c1a0e859
Nithya Sooriyapperuma <nithyamaduhansi@gmail.com>
2026-07-03 13:11:54 +0530

    Fix formatting issues in notificationController.js

--- BackEnd/controllers/notificationController.js
+++ BackEnd/controllers/notificationController.js
@@ -289,7 +289,7 @@ function getTimeAgo(date) {
   if (diff < 172800) return 'Yesterday';
   if (diff < 604800) return Math.floor(diff / 86400) + ' days ago';
   return Math.floor(diff / 604800) + ' weeks ago';
-}
+} 
 
 module.exports = {
   getNotifications,
