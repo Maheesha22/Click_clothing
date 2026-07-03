2e572977ec7e49b1b857ff558c71886e51541f90
Nithya Sooriyapperuma <nithyamaduhansi@gmail.com>
2026-07-03 13:19:19 +0530

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -26,6 +26,8 @@ const selectedItemsRoutes = require('./routes/SelectedItemsRoutes');
 const userAddressRoutes = require('./routes/userAddressRoutes');
 const dashboardRoutes = require('./routes/dashboardRoutes');
 const reportRoutes = require('./routes/reportRoutes');
+const notificationRoutes = require("./routes/notificationRoutes");
+
 const db = require('./models');
 
 const parseCorsOrigin = (originEnv) => {
@@ -104,7 +106,7 @@ app.use('/api/selected-items', selectedItemsRoutes);
 app.use('/api/user-addresses', userAddressRoutes);
 app.use('/api/dashboard', dashboardRoutes);
 app.use('/api/reports', reportRoutes);
-
+app.use("/api/notifications", notificationRoutes);
 app.get('/', (req, res) => {
   res.send('Click Clothing API is running.');
 });
