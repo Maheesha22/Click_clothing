4f3c2b149e8952de67f1f481e5927c6f4f95b71a
Maheesha nidushani <maheeshaecc18@gmail.com>
2026-07-04 16:26:46 +0530

    - changed the import issues in Trousers.jsx

--- FrontEnd/src/Pages/Trousers.jsx
+++ FrontEnd/src/Pages/Trousers.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import Header from "../Components/Header";
-import Footer from "../Components/Footer";
+import Header from "../Components/header";
+import Footer from "../Components/footer";
 import "./Trousers.css";
 import NavBar from "../Components/navsidebar"; 
 import WhatsAppButton from "../Components/whatsappbtn";
